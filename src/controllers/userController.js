1daf4f2915fbb53b3209d4c00823ab78c6b24c4f
DKhoa92 <dinhkhoa920410@gmail.com>
2023-05-08 22:06:17 +0700

    update login API

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,22 @@
-let handleLogin = (req, res) => {
+import userService from "../services/userService";
+
+let handleLogin = async (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
+
+    if (!email || !password) {
+        return res.status(500).json({
+            errCode: 1,
+            message: "Please input email, password",
+        })
+    }
+
+    let userData = await userService.handleUserLogin(email, password);
+
     return res.status(200).json({
-        message: "hello world",
-        yourEmail: email,
-        test: "test",
+        errCode: userData.errCode,
+        message: userData.errMessage,
+        user: userData.user ? userData.user : '',
     })
 }
 
