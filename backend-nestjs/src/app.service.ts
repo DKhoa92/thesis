b411e4b58ec8dec9ee0af8fe8d3f93bae4ee1798
NHTLong <42344711+NHTLong@users.noreply.github.com>
2023-10-22 21:18:31 +0700

    Backend CD (#7)
    * Update backend CD

--- backend-nestjs/src/app.service.ts
+++ backend-nestjs/src/app.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { execSync } from 'child_process';
+
+const executeGitCommand = (command) => {
+  return execSync(command)
+    .toString('utf8')
+    .replace(/[\n\r\s]+$/, '');
+};
 
 @Injectable()
 export class AppService {
   getAppInfo(): any {
     return {
       appName: 'Online Exam',
-      version: 'v1.0',
+      // version: 'v1.0',
+      git: {
+        branch: executeGitCommand('git rev-parse --abbrev-ref HEAD'),
+        commitHash: executeGitCommand('git rev-parse HEAD'),
+      },
     };
   }
 }
